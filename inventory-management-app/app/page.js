515132bddafb82e9bef2d52732bc1f52cf72eef0
abdunavv <abdnvd@gmail.com>
2024-11-25 17:32:10 -0500

    Update page.js

--- inventory-management-app/app/page.js
+++ inventory-management-app/app/page.js
@@ -17,10 +17,7 @@ const theme = createTheme({
       default: '#121212',
       paper: '#1e1e1e',
     },
-    text: {
-      primary: '#e0e0e0',
-      secondary: '#b0b0b0',
-    },
+    
     primary: {
       main: '#bb86fc',
     },
